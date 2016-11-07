6d214abb5e389dd31ee2a903a6e4b43a485794c0
Dominik Herold <d.herold@cid.biz>
2016-11-07 22:21:23 +0100

    add command line parsing to Program

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 ﻿using System;
 using System.IO;
 
+using CommandLineParser.Arguments;
+using CommandLineParser.Exceptions;
+
 namespace GreatFileReader
 {
     public class Program
     {
-        private static void Main()
+        private static void Main(string[] args)
         {
-            foreach (var line in File.ReadLines(@"C:\Projects\GreatFileReader\GreatFileReader.csproj"))
+            var parser = new CommandLineParser.CommandLineParser();
+            var fileName = new ValueArgument<string>('f', "file", "fileName");
+            parser.Arguments.Add(fileName);
+
+            try
+            {
+                parser.ParseCommandLine(args);
+            }
+            catch (CommandLineException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            foreach (var line in File.ReadLines(fileName.Value))
             {
                 Console.WriteLine(line);
                 Console.ReadKey();
